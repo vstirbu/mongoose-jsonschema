b097949233f32fac57fe720d8200f8d9d21b859b
Stéphane <stephane@attestis.com>
2020-12-08 14:07:10 +0100

    file definition in typescript

--- lib/local-types/mongoose-jsonschema/index.d.ts
+++ lib/local-types/mongoose-jsonschema/index.d.ts
@@ -1,12 +1,23 @@
+/// <reference path="../../index.js" />
+import * as mongoose from "mongoose";
+
+/**
+ * @alias module:mongoose-jsonschema
+ */
 declare module MongooseJsonSchema {
     /**
-     * @alias module:mongoose-jsonschema
-     * @param   {object}  model     Mongoose model to be converted
-     * @param   {object}  options   Options for customising model conversion
+     * @param   {mongoose.Model<any>}  model     Mongoose model to be converted
+     * @param   {any}  options   Options for customising model conversion
      * @param   {string[]|object} options.reserved - Model properties found in the array are not included in the schema or map of properties to be converted
      * @param   {boolean} options.reserved.property - Include/do not include model `property` into schema
-     * @returns {Object}  JSONSchema
+     * @returns {any}  JSONSchema
      */
-    function modelToJSONSchema(model, options): Object;
+    export function modelToJSONSchema(model: mongoose.Model<any>, options: any): any;
+    /**
+     * @param   {mongoose.Model<any>}  model     Mongoose model to be converted
+     * @returns {any}  JSONSchema
+     */
+    export function modelToJSONSchema(model: mongoose.Model<any>): any;
 }
-export = MongooseJsonSchema;
+
+export default MongooseJsonSchema;
